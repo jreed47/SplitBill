186b26d563cca1d9d05df4e2d29e9114c1353027
jreed47 <jonathan.reed@students.olin.edu>
2011-03-29 19:24:57 -0400

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SplitBill::Application.config.secret_token = '782fac4df4a04021e2f4f5bf756bcb5c2aecfb30f5e23df1cf5a86b875f595de34d1303c071947093e660db86262c7adf8d2ff6d475bba3f8dd09c4c7471c845'
